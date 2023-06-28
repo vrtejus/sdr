6d208a6ba3df9c3c4b24deb0b13fa69095a4ffff
Vishnu Rajan Tejus <vrtejus@gmail.com>
2023-06-28 11:36:09 -0700

    update stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,147 +1,15 @@
+import Head from "next/head";
 import React from "react";
 
 const SkylarWernethFacts = () => {
   return (
     <div>
-      <h2 className="text-2xl mb-6 mt-6 ml-6">The Skylar Werneth Facts</h2>
-      <ul className="list list-inside ml-10 mb-10 space-y-4 max-w-6xl">
-        <li>
-          When Skylar Werneth enters a room, phone reception improves by 300%.
-        </li>
-        <li>
-          Skylar Werneth's voicemail greeting has convinced 10% of callers to
-          buy his product without even speaking to him.
-        </li>
-        <li>
-          Skylar Werneth's cold calling skills are so exceptional that he once
-          convinced a wrong number to change their name legally just to match
-          his prospect list.
-        </li>
-        <li>
-          When Skylar Werneth sends a cold email, the recipient's inbox feels a
-          sudden warmth and irresistible urge to respond.
-        </li>
-        <li>
-          Skylar Werneth can perfectly imitate any accent or dialect, making him
-          the ultimate international cold calling expert.
-        </li>
-        <li>
-          During a cold call, Skylar Werneth accurately predicted a prospect's
-          objections before they even uttered a word.
-        </li>
-        <li>
-          Skylar Werneth's cold calling script has been permanently preserved in
-          the Smithsonian Institution as a masterpiece of salesmanship.
-        </li>
-        <li>
-          Skylar Werneth once closed a sale while giving a TED talk about the
-          art of cold calling.
-        </li>
-        <li>
-          Skylar Werneth's cold calling skills are so legendary that his name
-          has become a verb in the sales community: "Wernething" means closing
-          deals effortlessly.
-        </li>
-        <li>
-          Skylar Werneth's call logs are considered an essential reference for
-          salespeople worldwide, as they contain hidden secrets to successful
-          cold calling.
-        </li>
-        <li>
-          Skylar Werneth can recognize a potential customer's voice from a
-          single syllable, making him the fastest call qualifier in history.
-        </li>
-        <li>
-          Skylar Werneth's cold calling techniques have inspired a new field of
-          study known as "Wernethology."
-        </li>
-        <li>
-          During a cold call, Skylar Werneth discovered a hidden talent for
-          beatboxing and ended up signing a record deal with the prospect.
-        </li>
-        <li>
-          Skylar Werneth's voicemail messages have been transformed into
-          best-selling audiobooks, with listeners claiming they feel an
-          irresistible urge to buy after hearing his voice.
-        </li>
-        <li>
-          Skylar Werneth once made a cold call so persuasive that the prospect's
-          phone spontaneously transformed into a cash register and started
-          dispensing money.
-        </li>
-        <li>
-          When Skylar Werneth leaves a voicemail, it magically turns into a
-          30-second commercial jingle that gets stuck in the recipient's head
-          for weeks.
-        </li>
-        <li>
-          Skylar Werneth's cold calls have been known to cure hiccups,
-          migraines, and even fear of spiders in the middle of a conversation.
-        </li>
-        <li>
-          Once, Skylar Werneth cold called a prospect who happened to be an
-          extraterrestrial being. They immediately offered him exclusive rights
-          to market their advanced alien technology on Earth.
-        </li>
-        <li>
-          Skylar Werneth's cold calling prowess is so remarkable that the phrase
-          "as cool as a Werneth call" is now used to describe the smoothest,
-          most effortless sales pitches.
-        </li>
-        <li>
-          During a particularly challenging cold call, Skylar Werneth hypnotized
-          the prospect into buying his product, leading to a surge in demand for
-          his mesmerizing sales techniques.
-        </li>
-        <li>
-          Skylar Werneth's phone has a built-in feature that automatically
-          generates leads just by being in his presence.
-        </li>
-        <li>
-          Once, while on a cold call, Skylar Werneth accidentally cured a
-          prospect's fear of commitment, leading them to propose marriage on the
-          spot.
-        </li>
-        <li>
-          Skylar Werneth's cold calling sessions have been known to cause
-          temporary time dilation, making a 5-minute conversation feel like an
-          hour of captivating sales mastery.
-        </li>
-        <li>
-          During a cold call, Skylar Werneth accidentally convinced a
-          competitor's salesperson to quit their job and join his team, doubling
-          his company's revenue overnight.
-        </li>
-        <li>
-          Skylar Werneth's cold calling techniques are so effective that he once
-          closed a deal with a fax machine, causing it to print out a signed
-          contract all on its own.
-        </li>
-        <li>
-          When Skylar Werneth uses a phone booth for cold calling, it becomes a
-          teleportation device, instantly connecting him to prospects anywhere
-          in the world.
-        </li>
-        <li>
-          Skylar Werneth's voicemail messages have been known to cure insomnia,
-          with recipients falling into a deep sleep within seconds of hearing
-          his soothing voice.
-        </li>
-        <li>
-          Once, during a cold call, Skylar Werneth accidentally convinced the
-          prospect to hire him as their personal life coach, resulting in a
-          complete life transformation for both parties.
-        </li>
-        <li>
-          Skylar Werneth's cold calling skills are so extraordinary that he once
-          sold a product to a mime without uttering a single word.
-        </li>
-        <li>
-          During a cold call, Skylar Werneth's persuasive abilities caused the
-          prospect's pet parrot to recite their credit card number, completing
-          the sale.
-        </li>
-      </ul>
+      <Head>
+        <title>This page has a title 🤔</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h2 className="text-2xl mb-6 mt-6 ml-6">{"My Blog (Coming Soon)"}</h2>
     </div>
   );
 };
